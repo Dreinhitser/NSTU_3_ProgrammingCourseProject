c23770ec4894ff7fab107beb80459be71f7414f7
Денис Рихтер <rixter2001@mail.ru>
2023-04-22 07:38:18 +0700

    Добавил проверку методов и переопределенных операторов Fraction.

--- CourseProject/main.cpp
+++ CourseProject/main.cpp
@@ -1,17 +1,89 @@
 #include "Fraction.hpp"
 #include <iostream>
+#include <locale>
 
 int main()
 {
-	// todo написать тесты
-	Fraction f(11, 131);
-	Fraction f2(47, 53);
+	using namespace std;
+
+	setlocale(LC_ALL, "rus");
+
+	Fraction f1;
+	Fraction f2(3, 7);
+	Fraction f3(f2);
+
+	int numerator;
+	cout << "Введите числитель дроби: ";
+	cin >> numerator;
+	f1.set_numerator(numerator);
+
+	int denominator;
+	cout << "Введите знаменатель дроби: ";
+	cin >> denominator;
+	f1.set_denominator(denominator);
+
+	cout << "Первая дробь: ";
+	f1.display();
+	cout << "Вторая дробь: ";
+	f2.display();
+	cout << "Третья дробь: ";
+	f3.display();
+
+	// преобразование к базовым типам
+	// -----------------------------------
+	cout << "\n\n";
+	cout << "Перевод первой дроби в int: " << (int)f1 << "\n";
+	cout << "Перевод первой дроби в double: " << (double)f1 << "\n";
+	// -----------------------------------
+
+	// арифметические операции
+	// -----------------------------------
+	cout << "\n\n";
+	cout << "Операции с дробями:\n";
+	cout << "1) ";
+	f1.display();
+	cout << "2) ";
+	f2.display();
 
 	Fraction res;
 
-	res = f + f2;
-
+	res = f1 + f2;
+	cout << "Сумма дробей = ";
 	res.display();
+
+	res = f1 - f2;
+	cout << "Разность дробей = ";
+	res.display();
+
+	res = f1 / f2;
+	cout << "Частное дробей = ";
+	res.display();
+
+	res = f1 * f2;
+	cout << "Произведение дробей = ";
+	res.display();
+	// -----------------------------------
+
+	// сравнение дробей
+	// -----------------------------------
+	cout << "\n\n";
+	cout << "Сравнение дробей\nПоиск максимальной дроби\n";
+
+	if (f1 > f2)
+	{
+		cout << "Наибольшая дробь среди первых двух = ";
+		f1.display();
+	}
+	else if (f1 < f2)
+	{
+		cout << "Наибольшая дробь среди первых двух = ";
+		f2.display();
+	}
+	else
+	{
+		cout << "Дроби равны\n";
+	}
+	// -----------------------------------
 
 	return 0;
 }
